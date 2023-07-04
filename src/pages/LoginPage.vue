b71169a63b8ae329130074a0607a3e17babd4cb1
Azur冰弦 <973562770@qq.com>
2023-07-04 12:25:23 +0800

    Basic login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -39,14 +39,33 @@ function switchTab() {
   tab.value = tab.value == 'login' ? 'register' : 'login'
 }
 function onRegister() {
-
+  // TODO: Register logic
 }
 function onLogin() {
-
+  // TODO: Login logic
 }
 </script>
 
 <template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="200" height="200" />
+
+    <div class="wrapper">
+      <div class="greetings">
+        <h1 class="green">地铁客流量预测系统</h1>
+        <h3>
+          项目地址：
+          <a href="https://github.com/AzurIce/2023-BJTU-SummerPractice-Subway" target="_blank" rel="noopener">Github</a>
+        </h3>
+      </div>
+      <!-- <HelloWorld msg="You did it!" /> -->
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
   <main>
     <div class="flex flex-col gap-2">
       <v-sheet width="300" class="mx-auto">
@@ -73,7 +92,29 @@ function onLogin() {
   </main>
 </template>
 
-<style>
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
 .v-enter-active,
 .v-leave-active {
   transition: opacity 0.5s ease;
@@ -82,5 +123,67 @@ function onLogin() {
 .v-enter-from,
 .v-leave-to {
   opacity: 0;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
+import LoginPage from '../pages/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +13,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'login',
-      component: LoginView
+      component: LoginPage
     },
     {
       path: '/about',
