8072eb85b4b35d42c18470347cfd90de6aaadb79
Azur冰弦 <973562770@qq.com>
2023-07-04 09:53:23 +0800

    Added element-plus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(ElementPlus)
+    .mount('#app')
