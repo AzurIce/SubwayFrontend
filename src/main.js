32a2f6df35b8576a86429f7ff2d2013e59ea2ddd
Azur冰弦 <973562770@qq.com>
2023-07-05 11:03:59 +0800

    Basic subway route in MapBox

--- src/main.js
+++ src/main.js
@@ -10,14 +10,9 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 const vuetify = createVuetify()
 
-import Vue3BaiduMapGL from 'vue3-baidu-map-gl'
-
 createApp(App)
     .use(createPinia())
     .use(router)
     .use(vuetify)
-    .use(Vue3BaiduMapGL, {
-        ak: 'KG6YhqSuGhx192cRVdgHzvHHG7TI0dua'
-    })
 
     .mount('#app')
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,29 +1,53 @@
 <script setup>
   import 'mapbox-gl/dist/mapbox-gl.css';
-  import { BMap, BZoom } from 'vue3-baidu-map-gl'
-  import { onMounted, reactive } from 'vue'
+  // import subway_lines_data from '../assets/Subway Lines.geojson'
+  import { onMounted } from 'vue'
 
   import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiYXp1cmljZSIsImEiOiJjbGpwMnQxcXIxYTNpM2VvNmo0OG14cm1xIn0.dc0RFy9AhS-sxmtnvdI4Qw';
 
+  let map = null
+
   onMounted(() => {
-    const map = new mapboxgl.Map({
+    map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v12', // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
+        // center: [-73.904496, 40.720449], // starting position [lng, lat]
+        maxBounds: [
+          [-74.309883, 40.48388],
+          [-73.677476, 40.899622]
+        ],
         zoom: 9, // starting zoom
     });
-  })
 
-
-  const center = reactive({
-    lng: -73.904496,
-    lat: 40.720449
+    map.on('load', () => {
+      map.addSource('route', {
+        'type': 'geojson',
+        'data': 'Subway Lines.geojson'
+      })
+      map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+        },
+        'paint': {
+        'line-color': '#888',
+        'line-width': 8
+        }
+      })
+    })
   })
+  // -73.913192,40.903221
+  // -73.764864,40.533469
+
 </script>
 
 <template>
+  <!-- <button @click="() => {map.setCenter([0, 0])}">asddasdasd</button> -->
   <div id="map" class="tw-h-full tw-w-full"/>
   <!-- <BMap
     :height="'100%'"
