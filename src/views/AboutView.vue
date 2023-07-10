24af7e185886b08836ce15cafba0aa62ceca7aa3
许一涵 <81723334@qq.com>
2023-07-10 22:15:48 +0800

    添加了about页面

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,72 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <particles :color="['#ff0000', '#00ff00', '#0000ff']"
+                :particleOpacity="0.7"
+                :particlesNumber="200"
+                :shapeType="'circle'"
+                :particleSize="3"
+                :linesColor="'#ffffff'"
+                :linesWidth="1"
+                :lineLinked="true"
+                :lineOpacity="0.4"
+                :linesDistance="150"
+                :moveSpeed="3"
+                :hoverEffect="true"
+                :hoverMode="'grab'"
+                :clickEffect="true"
+                :clickMode="'push'"
+      ></particles> -->
+    <h1 class="green tw-text-center">北京交通大学软件学院2023暑期实训项目</h1>
+    <h2 class="green">自来熟小组</h2>
+    <h2 class="green">组员有：xyh，xb，cyb，fwh，cjx，zml</h2>
+    <v-btn class="green" append-icon="$vuetify" stacked @click="addCount">
+      Button
+    </v-btn>
+    <h1 class="green">你一共点击了{{ count }}次</h1>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    addCount() {
+      this.count++;
+    },
+  },
+}
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+h1 {
+  font-weight: 500;
+  font-size: 2.3rem;
+  position: relative;
+  top: -10px;
+}
+
+h2 {
+  font-weight: 500;
+  font-size: 1.5rem;
+  position: relative;
+  top: -10px;
+}
+
+.about {
+  background-image: url('../assets/308073.jpg');
+  text-align: center;
+  align-items: center;
+  position: absolute;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
 }
 </style>
