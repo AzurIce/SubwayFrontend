3f633c16ad864de176e530668fafcc86a8c77d02
Azur冰弦 <973562770@qq.com>
2023-07-11 14:59:40 +0800

    fix: show me money icon

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -21,26 +21,20 @@ import MapBox from '../components/MapBox.vue'
 </script>
 
 <template>
+  <v-dialog v-model="donateDialog" width="auto">
+    <v-card>
+      <v-img src="src/assets/showMeMoney.jpg" contain height="300"></v-img>
+      <v-card-text>
+        我很可爱请给我钱
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" block @click="donateDialog = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
   <v-layout class="tw-h-full">
     <v-app-bar :elevation="2" title="地铁客流量预测系统">
-      <v-btn icon="mdi-heart">
-          $ 
-          <v-dialog 
-            v-model="donateDialog"
-            activator="parent"
-            width="auto"
-          >
-            <v-card>
-              <v-img src="src/assets/showMeMoney.jpg" contain height="300"></v-img>
-              <v-card-text>
-                我很可爱请给我钱
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="primary" block @click="donateDialog = false">Close</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-      </v-btn>
+      <v-btn icon="mdi-currency-usd" @click="() => {donateDialog = true}" />
       <v-btn icon="mdi-poll"></v-btn>
       <v-btn icon="mdi-exit-run" @click="onLogout()"></v-btn>
     </v-app-bar>
