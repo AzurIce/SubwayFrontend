5394fbe680adefa94b7891291cddbae769ed06fe
Cybing521 <1649192259@qq.com>
2023-07-11 15:03:31 +0800

    Make the navigation bar more independent

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,14 +3,13 @@ import router from '../router/index'
 import { ref } from 'vue'
 
 // let map = null
-const donateDialog = ref(false)
+
 // import { getData } from '../lib/gtfs/gtfs'
 // import axios from 'axios';
 // import stops_data from './stops.json'
 // import stops_data from '../assets/stops.json'
 // console.log(stops_data)
-import { useTokenStore } from '../stores/token'
-const tokenStore = useTokenStore()
+
 
 // onMounted(() => {
 //   setTimeout(() => (dialog.value = false), 2500)
@@ -19,41 +18,16 @@ const tokenStore = useTokenStore()
 // -73.764864,40.533469
 
 
-function onLogout() {
-  tokenStore.unSetToken()
-  router.push('/login')
-}
-
 // async function onGetData() {
 //   const geojson = getData()
 // }
 import MapBox from '../components/MapBox.vue'
+import NavigationBar from '../components/NavigationBar.vue';
 </script>
 
 <template>
   <v-layout class="tw-h-full">
-    <v-app-bar :elevation="2" title="地铁客流量预测系统">
-      <v-btn icon="mdi-heart">
-          $ 
-          <v-dialog 
-            v-model="donateDialog"
-            activator="parent"
-            width="auto"
-          >
-            <v-card>
-              <v-img src="src/assets/showMeMoney.jpg" contain height="300"></v-img>
-              <v-card-text>
-                我很可爱请给我钱
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="primary" block @click="donateDialog = false">Close</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-      </v-btn>
-      <v-btn icon="mdi-poll"></v-btn>
-      <v-btn icon="mdi-exit-run" @click="onLogout()"></v-btn>
-    </v-app-bar>
+    <NavigationBar></NavigationBar>
     <v-main class="tw-h-full">
       <MapBox />
     </v-main>
