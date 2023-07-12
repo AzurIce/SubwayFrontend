84edd494603be4b5e4ae38619932beb386b0ac41
许一涵 <110003881+XuYiHan30319@users.noreply.github.com>
2023-07-12 14:46:16 +0800

    update host

--- src/lib/axios/axios.js
+++ src/lib/axios/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-// const host = 'http://192.168.43.128:3308'
+const host = 'http://192.168.43.128:3308'
 // const host = 'http://123.60.53.131:3308'
 // const host = 'http://120.46.129.79:3308'
-const host = 'http://124.70.109.243:3308'
+// const host = 'http://124.70.109.243:3308'
 
 export function get(url) {
   return axios.get(`${host}${url}`, {
