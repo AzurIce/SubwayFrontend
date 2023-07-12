eeddbbf7316f97a9f20f84468fffc111bac6e62d
许一涵 <110003881+XuYiHan30319@users.noreply.github.com>
2023-07-12 09:57:47 +0800

    导航栏美化

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -29,11 +29,18 @@ function onLogout() {
       </v-card-actions>
     </v-card>
   </v-dialog>
-  <v-app-bar :elevation="2" title="地铁客流量预测系统">
+  <v-app-bar class="bar" :elevation="2"  image="src/assets/NavigationBar2.png" >
+    <v-app-bar-nav-icon  @click="() => { router.push('/about') }">
+      <img src="src/assets/NavigationBar-icon.png" alt="Custom Icon">
+    </v-app-bar-nav-icon>
+    <v-toolbar-title>地铁客流量预测系统</v-toolbar-title>
     <v-btn class="tw-text-purple" v-if="tokenStore.isAdmin()" @click="() => { router.push('/admin') }">管理</v-btn>
     <v-btn icon="mdi-currency-usd" @click="() => { donateDialog = true }" />
     <v-btn icon="mdi-poll" @click="() => { router.push('/count') }"></v-btn>
     <v-btn icon="mdi-exit-run" @click="onLogout()"></v-btn>
   </v-app-bar>
 </template>
-<style scoped></style>
+
+<style scoped>
+</style>
+
